3c5a3ebf306d3060f3bc5d8d80967856f450b5e6
enigmagnetic <lauren.cardella@gmail.com>
2017-10-26 03:42:43 -0700

    Add require_login before action for all but root and login pages. Flash message is not showing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  before_action :require_login
 
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    if current_user.nil?
+      flash[:error] = "You must be logged in to view this page."
+      redirect_to root_path
+    end
   end
 
 private
